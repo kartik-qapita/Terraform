351bdef8a4fb58195a6c123cb90c28d24713c39a
kartik-qapita <97021175+kartik-qapita@users.noreply.github.com>
2022-01-21 16:19:03 +0530

    Softwares added

--- mount-and-install.sh
+++ mount-and-install.sh
@@ -60,8 +60,122 @@ sudo mount -a
 echo
 sudo mount -a
 echo
-#INSTALL-SOFTWARE: COMMANDS
-#NGINX-INSTALLATION:
+
+# Setting up the Software Components
+# Installing - Docker, SEQ-Data, MongoDB, EventStore, Nginx, .NET SDK 6.0
+# DOCKER
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install SEQ
+sudo docker image pull datalust/seq
+sudo docker container create -p 5341:5341 -p 8081:80 \
+    -v /seq-data:/data \
+    -e ACCEPT_EULA=y \
+    --name q-seq-node datalust/seq
+sudo docker container start q-seq-node
+
+# Installing MongoDB
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+# You can verify that mongodb is installed by the following command
+type -a mongod
+# Configuration file of mongo is in /etc/mongod.conf, set storage.dbPath to /mongodb-data/db
+# Set storage.directoryPerDB to true, set systemLog.path to /mongodb-data/log/mongod.log
+sudo mkdir -p /mongodb-data/db /mongodb-data/log /mongodb-data/journal
+sudo ln -s /mongodb-data/journal /mongodb-data/db/journal
+# Change ownership of /mongodb-data folder to mongodb user
+sudo chown -R mongodb:mongodb /mongodb-data
+# Start mongodb
+sudo systemctl start mongod
+# To check if mongodb is running
+sudo systemctl status mongod
+# Make mongod start automatically when machine starts
+sudo systemctl enable mongod
+
+# Installing DotNet-6.0
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb rm packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+
+# Install EventStoreDB
+# Install aws cli (to download EventStoreDB from s3 folder)
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Download the eventstore and related files from aws s3 bucket
+mkdir -p ~/machine-setup/eventstore ~/machine-setup/certificates
+aws s3 cp s3://qapita-development/EventStore ~/machine-setup/eventstore --recursive
+aws s3 cp s3://qapita-development/certificates ~/machine-setup/certificates --recursive
+sudo dpkg -i ~/machine-setup/eventstore/EventStore-Commercial-Linux-v20.6.1-2.ubuntu-18.04.deb
+sudo mkdir eventstoredb
+# Make eventstore user as the owner of /eventstore
+sudo cp ~/machine-setup/eventstore/eventstore-1.pfx /etc/eventstore
+sudo cp ~/machine-setup/eventstore/eventstore.conf /etc/eventstore
+sudo cp ~/machine-setup/certificates/qapita-CA.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
+sudo chown -R eventstore:eventstore /etc/eventstore
+sudo chown -R eventstore:eventstore /eventstoredb
+
+# Configure the eventstore in /etc/eventstore/eventstore.conf
+# Replace eventstore.conf contents with the following lines
+echo '
+RunProjections: All
+ClusterSize: 1
+MemDb: False
+StartStandardProjections: True
+EnableExternalTcp: True
+Db: /eventstoredb-data/db
+Log: /eventstoredb-data/log
+EnableAtomPubOverHttp: True
+ExtIp: 0.0.0.0
+IntIp: 0.0.0.0
+CertificateFile: /etc/eventstore/eventstore-1.pfx
+CertificatePassword: qapita
+TrustedRootCertificatesPath: /usr/local/share/ca-certificates/ ' | sudo tee -a /etc/eventstore/eventstore.conf
+
+sudo cp ~/machine-setup/certificates/qapita-CA.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
+# Start the eventstore
+sudo systemctl start eventstore
+# Check status of eventstore
+sudo systemctl status eventstore
+# Make eventstore start automatically when the machine starts
+sudo systemctl enable eventstore
+
+# Installing NGINX 1.20.2
+(cat << EOF
+deb https://nginx.org/packages/ubuntu/ $(lsb_release -cs) nginx
+deb-src https://nginx.org/packages/ubuntu/ $(lsb_release -cs) nginx
+EOF
+) | sudo tee /etc/apt/sources.list.d/nginx.list
+sudo apt-get update
+# If key error, copy the key to environment variable "key" and run the below command
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
+sudo apt-get remove -y nginx
 sudo apt install -y nginx
-sudo service nginx start
-echo
+echo 'Software Installation Completed'
+# Softwares Installation Completed.
