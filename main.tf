43aebb521683d50c33c59f8b203adaf74f085934
kartik-qapita <97021175+kartik-qapita@users.noreply.github.com>
2022-01-21 19:53:28 +0530

    Added Role & Name of EC2

--- main.tf
+++ main.tf
@@ -12,25 +12,27 @@ terraform {
 provider "aws" {
   access_key = "" # ADD AWS_ACCESS_KEY
   secret_key = "" # ADD AWS_SECRET_KEY
-  region     = "ap-south-1"
+  region     = "ap-south-1" # Mumbai Region
 }
 
 #Resource-1 : ebs_volume.tf
 #DATA : Bash Script
 data "template_file" "userdata" {
-  template = file("mount-and-install.sh")
+  template = file("mount-and-install.sh") # Script File Name
 }
 
 #EC2 Instance : 
 resource "aws_instance" "phantoms-dev-instance" {
-  ami                         = "ami-08ee6644906ff4d6c" #ubuntu 18.04LTS
+  ami                         = "ami-08ee6644906ff4d6c"              # ubuntu 18.04LTS
   availability_zone           = "ap-south-1b"
-  instance_type               = "t2.micro" #FREE TIER
-  subnet_id                   = "subnet-"#ADD SUBNET_ID  #PRIVATE SUBNET
-  vpc_security_group_ids      = [""] # ADD SECURITY_GROUP_ID 
-  associate_public_ip_address = true                                 #ASSIGNS PUBLIC IP TO INSTANCE 
-  user_data                   = data.template_file.userdata.rendered #RUNS OUR SCRIPT
+  instance_type               = "t2.micro"                           # FREE TIER
+  subnet_id                   = "subnet-"                            # ADD SUBNET_ID  #PRIVATE SUBNET
+  vpc_security_group_ids      = [""]                                 # ADD SECURITY_GROUP_ID
+  iam_instance_profile        = "ec2-role"                           # Assigns IAM Role
+  associate_public_ip_address = true                                 # ASSIGNS PUBLIC IP TO INSTANCE 
+  user_data                   = data.template_file.userdata.rendered # RUNS OUR SCRIPT
   tags = {
     environment = "development"
+    Name        = "dev-instance"                                     # Assigns Name to EC2 Instances
   }
 }
